fac65a73a9fc630f1813c16b30b79391cdc14771
99mini <itman390@gmail.com>
2025-04-28 17:57:22 +0900

    feat: update package.json and rollup config for improved build process; add tsconfig.build.json for type declarations

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,33 @@ import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
+import pkg from "./package.json" assert { type: "json" };
+
 export default {
   input: "src/index.ts",
   output: [
-    { file: "dist/index.cjs.js", format: "cjs" },
-    { file: "dist/index.esm.js", format: "esm" },
+    {
+      file: pkg.main, // "main": "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module, // "module": "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
   ],
   plugins: [
     resolve(),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
     babel({
       babelHelpers: "bundled",
       extensions: [".ts"],
       exclude: "node_modules/**",
     }),
   ],
-  external: ["typescript"],
+  external: [], // 외부 의존성은 여기에 명시
 };
