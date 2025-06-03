d42bdab442c239c8e8a06a238672f23e382e2f19
99mini <43674669+99mini@users.noreply.github.com>
2025-06-03 14:11:33 +0900

    feat: test coverage (#28)
    * test: revert test code
    
    * revert: revert test code
    
    * revert: revert test code
    
    * chore: fix json
    
    * test: coverage 100

--- src/lazy/index.spec.ts
+++ src/lazy/index.spec.ts
@@ -4,15 +4,15 @@ describe("lazy index exports", () => {
   it("should export all lazy functions", () => {
     expect(lazyExports.chunk).toBeDefined();
     expect(lazyExports.filter).toBeDefined();
-    // expect(lazyExports.flatMap).toBeDefined();
-    // expect(lazyExports.groupBy).toBeDefined();
-    // expect(lazyExports.map).toBeDefined();
-    // expect(lazyExports.partition).toBeDefined();
-    // expect(lazyExports.pluck).toBeDefined();
-    // expect(lazyExports.range).toBeDefined();
-    // expect(lazyExports.scan).toBeDefined();
-    // expect(lazyExports.sortBy).toBeDefined();
-    // expect(lazyExports.uniq).toBeDefined();
-    // expect(lazyExports.zip).toBeDefined();
+    expect(lazyExports.flatMap).toBeDefined();
+    expect(lazyExports.groupBy).toBeDefined();
+    expect(lazyExports.map).toBeDefined();
+    expect(lazyExports.partition).toBeDefined();
+    expect(lazyExports.pluck).toBeDefined();
+    expect(lazyExports.range).toBeDefined();
+    expect(lazyExports.scan).toBeDefined();
+    expect(lazyExports.sortBy).toBeDefined();
+    expect(lazyExports.uniq).toBeDefined();
+    expect(lazyExports.zip).toBeDefined();
   });
 });
